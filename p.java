c52727f1cc125f9768cd463e248696219742156e
sandeep <sandeepvairagi818@gamil.com>
2026-01-13 20:38:11 +0530

    java code

---
+++ p.java
@@ -0,0 +1,10 @@
+public class p{
+    public static void main(String[] args) {
+        int num1 = 10;   // first number
+        int num2 = 20;   // second number
+
+        int sum = num1 + num2;  // addition
+
+        System.out.println("The sum is: " + sum);
+    }
+}
